36a525c91df61d4eb125b8649377636515454c04
rdtHK <mariop.36754@gmail.com>
2016-07-07 18:34:07 -0300

    Adding bootstrap-sass to the project.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var path = require("path");
+
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + "/app/index.html",
@@ -5,20 +7,48 @@ var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   inject: "body"
 });
 
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var extractCSS = new ExtractTextPlugin('css/[contenthash]-main.css');
+
 module.exports = {
   entry: [
     "./app/index.js"
   ],
   output: {
     path: __dirname + "/dist",
-    filename: "index_bundle.js"
+    filename: "js/[hash]-bundle.js"
   },
+  // devtool: "inline-source-map",
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    }]
+    loaders: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/i,
+        loader: extractCSS.extract(["css?sourceMap"])
+      },
+      {
+        test: /\.scss$/i,
+        loader: extractCSS.extract(["css?sourceMap", "sass?sourceMap"])
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          "file?name=img/[hash]-[name].[ext]",
+          "image-webpack"
+        ]
+      },
+      {
+        test: /\.(eot|woff2?|ttf)$/i,
+        loader: "file?name=font/[hash]-[name].[ext]"
+      }
+    ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    HtmlWebpackPluginConfig,
+    extractCSS
+  ]
 };
